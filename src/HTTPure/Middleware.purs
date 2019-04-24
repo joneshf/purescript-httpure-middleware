4b969eb5534aec1901b5dfa3f4247ba794abaa01
joneshf <jones3.hardy@gmail.com>
2019-04-24 09:37:42 -0700

    Add development log middleware
    While the Apache log formats are really good for production,
    it's also nice to have something a bit more readable in development.
    
    This middleware extracts a bunch of information from the request/response,
    and formats it in a way that should be a little easier to decipher.

--- src/HTTPure/Middleware.purs
+++ src/HTTPure/Middleware.purs
@@ -1,19 +1,27 @@
 module HTTPure.Middleware
   ( combinedLogFormat
   , commonLogFormat
+  , developmentLogFormat
   ) where
 
 import Prelude
 
+import Ansi.Codes as Ansi.Codes
+import Ansi.Output as Ansi.Output
 import Data.Array as Data.Array
 import Data.DateTime as Data.DateTime
+import Data.FoldableWithIndex as Data.FoldableWithIndex
 import Data.Formatter.DateTime as Data.Formatter.DateTime
+import Data.Int as Data.Int
 import Data.Maybe as Data.Maybe
 import Data.String as Data.String
+import Data.Time.Duration as Data.Time.Duration
 import Effect.Class as Effect.Class
 import Effect.Class.Console as Effect.Class.Console
 import Effect.Now as Effect.Now
+import Foreign.Object as Foreign.Object
 import HTTPure as HTTPure
+import HTTPure.Headers as HTTPure.Headers
 
 type Middleware
   = (HTTPure.Request -> HTTPure.ResponseM) -> HTTPure.Request -> HTTPure.ResponseM
@@ -66,6 +74,77 @@ commonLogFormat router request = do
     )
   pure response
 
+colorMethod :: HTTPure.Method -> String
+colorMethod method = Ansi.Output.withGraphics graphics (renderMethod method)
+  where
+  graphics = case method of
+    HTTPure.Connect -> Ansi.Output.foreground Ansi.Codes.Blue
+    HTTPure.Delete -> Ansi.Output.foreground Ansi.Codes.Red
+    HTTPure.Get -> Ansi.Output.foreground Ansi.Codes.Cyan
+    HTTPure.Head -> Ansi.Output.foreground Ansi.Codes.Cyan
+    HTTPure.Options -> Ansi.Output.foreground Ansi.Codes.Blue
+    HTTPure.Patch -> Ansi.Output.foreground Ansi.Codes.Magenta
+    HTTPure.Post -> Ansi.Output.foreground Ansi.Codes.Yellow
+    HTTPure.Put -> Ansi.Output.foreground Ansi.Codes.Green
+    HTTPure.Trace -> Ansi.Output.foreground Ansi.Codes.Blue
+
+colorStatus :: HTTPure.Status -> String
+colorStatus status = Ansi.Output.withGraphics graphics (renderStatus status)
+  where
+  graphics
+    | status < 200 = Ansi.Output.foreground Ansi.Codes.Blue
+    | 200 <= status && status < 300 = Ansi.Output.foreground Ansi.Codes.Green
+    | 300 <= status && status < 400 = Ansi.Output.foreground Ansi.Codes.Cyan
+    | 400 <= status && status < 500 = Ansi.Output.foreground Ansi.Codes.BrightYellow
+    | otherwise = Ansi.Output.foreground Ansi.Codes.Red
+
+-- | A middleware that logs request in an unstandardized development format.
+-- | The logs are more verbose, colorful, and a bit easier to read.
+developmentLogFormat :: Middleware
+developmentLogFormat router request = do
+  start <- Effect.Class.liftEffect Effect.Now.nowDateTime
+  response <- router request
+  stop <- Effect.Class.liftEffect Effect.Now.nowDateTime
+  Effect.Class.Console.log
+    ( Data.String.joinWith "\n"
+      ( method
+        <> object "Query" request.query
+        <> body
+        <> object "Headers" headers
+        <> duration stop start
+        <> status response.status
+      )
+    )
+  pure response
+  where
+  body
+    | hasBody request.method = ["  " <> white "Body" <> ": " <> request.body]
+    | otherwise = []
+  duration stop start =
+    [ "  "
+      <> white "Duration"
+      <> ": "
+      <> renderDuration (Data.DateTime.diff stop start)
+    ]
+  method =
+    [colorMethod request.method <> " /" <> Data.String.joinWith "/" request.path]
+  object name obj
+    | not Foreign.Object.isEmpty obj =
+      [ "  " <> white name <> ":"
+      ] <> flip Data.FoldableWithIndex.foldMapWithIndex obj \key val ->
+        ["    " <> white key <> ": " <> val]
+    | otherwise = []
+  status x = ["  " <> white "Status" <> ": " <> colorStatus x]
+  white = Ansi.Output.withGraphics (Ansi.Output.foreground Ansi.Codes.White)
+  (HTTPure.Headers.Headers headers) = request.headers
+
+hasBody :: HTTPure.Method -> Boolean
+hasBody = case _ of
+  HTTPure.Patch -> true
+  HTTPure.Post -> true
+  HTTPure.Put -> true
+  _ -> false
+
 renderDateTime :: Data.DateTime.DateTime -> String
 renderDateTime =
   Data.Formatter.DateTime.format
@@ -86,6 +165,10 @@ renderDateTime =
       , Data.Formatter.DateTime.Placeholder "]"
       ]
     )
+
+renderDuration :: Data.Time.Duration.Milliseconds -> String
+renderDuration = case _ of
+  Data.Time.Duration.Milliseconds x -> show (Data.Int.round x) <> "ms"
 
 renderMethod :: HTTPure.Method -> String
 renderMethod = case _ of
@@ -119,6 +202,73 @@ renderRequest { method, path } =
 renderSize :: HTTPure.Headers -> String
 renderSize headers =
   Data.Maybe.fromMaybe unknown (HTTPure.lookup headers "Content-Length")
+
+renderStatus :: HTTPure.Status -> String
+renderStatus status = show status <> " " <> human
+  where
+  human = case status of
+    100 -> "Continue"
+    101 -> "Switching Protocols"
+    102 -> "Processing"
+    200 -> "Ok"
+    201 -> "Created"
+    202 -> "Accepted"
+    203 -> "Non Authoritative Information"
+    204 -> "No Content"
+    205 -> "Reset Content"
+    206 -> "Partial Content"
+    207 -> "Multi Status"
+    208 -> "Already Reported"
+    226 -> "IM Used"
+    300 -> "Multiple Choices"
+    301 -> "Moved Permanently"
+    302 -> "Found"
+    303 -> "See Other"
+    304 -> "Not Modified"
+    305 -> "Use Proxy"
+    307 -> "Temporary Redirect"
+    308 -> "Permanent Redirect"
+    400 -> "Bad Request"
+    401 -> "Unauthorized"
+    402 -> "Payment Required"
+    403 -> "Forbidden"
+    404 -> "Not Found"
+    405 -> "Method Not Allowed"
+    406 -> "Not Acceptable"
+    407 -> "Proxy Authentication Required"
+    408 -> "Request Timeout"
+    409 -> "Conflict"
+    410 -> "Gone"
+    411 -> "Length Required"
+    412 -> "Precondition Failed"
+    413 -> "Payload Too Large"
+    414 -> "URI Too Long"
+    415 -> "Unsupported Media Type"
+    416 -> "Range Not Satisfiable"
+    417 -> "Expectation Failed"
+    418 -> "Im A Teapot"
+    421 -> "Misdirected Request"
+    422 -> "Unprocessable Entity"
+    423 -> "Locked"
+    424 -> "Failed Dependency"
+    426 -> "Upgrade Required"
+    428 -> "Precondition Required"
+    429 -> "Too Many Requests"
+    431 -> "Request Header Fields Too Large"
+    451 -> "Unavailable For Legal Reasons"
+    500 -> "Internal Server Error"
+    501 -> "Not Implemented"
+    502 -> "Bad Gateway"
+    503 -> "Service Unavailable"
+    504 -> "Gateway Timeout"
+    505 -> "HTTP Version Not Supported"
+    506 -> "Variant Also Negotiates"
+    507 -> "Insufficient Storage"
+    508 -> "Loop Detected"
+    510 -> "Not Extended"
+    511 -> "Network Authentication Required"
+    _ -> ""
+
 
 renderUserAgent :: HTTPure.Request -> String
 renderUserAgent { headers } = show (HTTPure.at headers "User-Agent")
